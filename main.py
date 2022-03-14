f1fcec48dc22020551113764769e29d24d656979
Fidel Luis Inacio Lacasse <fluis.lacasse@gmail.com>
2022-03-14 07:44:57 +0200

    Adjustments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
-from numpy import mean, std
+from numpy import mean, asarray, std
 
 import persistence
 
@@ -66,6 +66,7 @@ def update_results():
 
 
 def predict_today():
+    print('Começou a predição')
     tod = matchservices.get_today_matches()
     results = []
     for i in tod:
--- training.py
+++ training.py
@@ -1,9 +1,13 @@
-import pandas
+from numpy.random import seed
 
+seed(1)  # keras seed fixing import
+
+import tensorflow
+
+tensorflow.random.set_seed(2)  # tensorflow seed fixing
 
 # mlp for multi-label classification
 import pandas as pd
-from numpy import mean, asarray, std
 from sklearn.model_selection import RepeatedKFold
 from keras.models import Sequential
 from keras.layers import Dense
@@ -12,7 +16,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 def get_training_data():
-    dataframe = pandas.read_csv("persistence/training.csv")
+    dataframe = pd.read_csv("persistence/training.csv")
     dataset = dataframe.drop('awardedMatches', axis=1).drop('id', axis=1).values
     X = dataset[:, 0:98].astype(float)
     y = dataset[:, 98:].astype(int)
@@ -24,13 +28,13 @@ def get_model(n_inputs, n_outputs):
     model.add(Dense(90, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
     model.add(Dense(n_outputs, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam')
-    # model.fit(X, y, epochs=150, batch_size=10)
     return model
 
 
 def evaluate_model(X, y):
     results = list()
     n_inputs, n_outputs = X.shape[1], y.shape[1]
+    model = get_model(n_inputs, n_outputs)
     # define evaluation procedure
     cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
     # enumerate folds
@@ -38,15 +42,15 @@ def evaluate_model(X, y):
         # prepare data
         X_train, X_test = X[train_ix], X[test_ix]
         y_train, y_test = y[train_ix], y[test_ix]
-        # define model
-        model = get_model(n_inputs, n_outputs)
         st_x = MinMaxScaler()
         X_train = st_x.fit_transform(X_train)
         X_test = st_x.transform(X_test)
         # fit model
-        model.fit(X_train, y_train, verbose=0, epochs=1000)
+        model.fit(X_train, y_train, verbose=0, epochs=200)
         # make a prediction on the test set
         yhat = model.predict(X_test)
+        # print(yhat)
+        # print(y_test)
         # round probabilities to class labels
         yhat = yhat.round()
         # calculate accuracy
@@ -65,12 +69,12 @@ def make_prediction(mat: dict) -> []:
     # fit the model on all data
     st_x = MinMaxScaler()
     X = st_x.fit_transform(X)
-    model.fit(X, y, verbose=0, epochs=1000)
+    model.fit(X, y, verbose=0, epochs=200)
     # make a prediction for new data
     # row = [3, 3, 6, 7, 8, 2, 11, 11, 1, 3]
     mat = pd.DataFrame([mat])
     dataset = mat.drop('awardedMatches', axis=1).drop('id', axis=1).values
-    newX = dataset[:,0:98].astype(float)
+    newX = dataset[:, 0:98].astype(float)
     newX = st_x.transform(newX)
     yhat = model.predict(newX)
     return yhat[0]
